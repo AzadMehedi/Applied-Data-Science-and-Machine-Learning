05948f168aee120a570225c1da952358d5a108e6
Mehedi Azad <49702660+AzadMehedi@users.noreply.github.com>
2025-10-24 03:30:50 +0000

    Set done

---
+++ Set/Basic_Set.py
@@ -0,0 +1,7 @@
+# A simple demonstration of creating and printing a set.
+
+fruits = {'apple', 'banan', 'cherry'}
+
+if __name__ == '__main__':
+    print('Fruits set: ', fruits)
+    print('Number of elements: ', len(fruits))
---
+++ Set/Set_from_String.py
@@ -0,0 +1,7 @@
+# Convert strings to sets to find unique characters.
+
+word = 'Banana'
+unique_letters = set(word)
+
+if __name__ == '__main__':
+    print("Unique letters in 'Banana' : ", unique_letters)
